5fe5014108e0eff9d3b09c1fc011039497d8c3e1
chenshijie <chenshijie@syvip.com>
2018-01-07 13:37:25 +0800

    头部样式修改

--- q-yue/src/components/header.vue
+++ q-yue/src/components/header.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="header">
-    <div class="header-icon" v-show="backDisplay" @click="goBack"><i class="icon">&#xe622;</i></div>
+    <div class="header-icon iconStyle01" v-show="backDisplay" @click="goBack"><i class="icon">&#xe622;</i></div>
     <div class="header-cont"><p>{{title}}</p></div>
     <div class="header-icon" v-show="menuDisplay" @click="showBar"><i class="icon">&#xe634;</i></div>
-    <div class="header-icon" v-show="mapDisplay" @click="getMap"><i class="icon map-icon">&#xe600;</i></div>
+    <div class="header-icon iconStyle02" v-show="mapDisplay" @click="getMap"><i class="icon map-icon">&#xe600;</i></div>
   </div>
 </template>
 
@@ -46,9 +46,16 @@
     background: #76D49B;
     display: flex;
     flex-direction: row;
+    .iconStyle01{
+      margin-left:20px;
+    }
+    .iconStyle02{
+      margin-right:20px;
+    }
     .header-icon {
-      flex: 1;
-      text-align: center;
+      // flex: 1;
+      // text-align: right;
+      margin-right:20px;
       > i {
         line-height: px2rem(200px);
       }
@@ -68,9 +75,9 @@
     }
   }
 
-  @media screen and (min-width: 930px){
+  /* @media screen and (min-width: 930px){
     .header-cont {
       padding-left: px2rem(300px) !important;
     }
-  }
+  } */
 </style>
